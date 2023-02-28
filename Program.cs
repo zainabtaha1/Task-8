13c69a2c11b88652b8996901dc1658118969e881
zainabtaha1 <121588138+zainabtaha1@users.noreply.github.com>
2023-02-28 04:07:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Problem 1 in  section 3
+            Console.WriteLine(" Enter the first number ");
+            double firstnum = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Enter the second number ");
+            double secondnum = double.Parse(Console.ReadLine());
+            double result;
+            result = firstnum + secondnum;
+            Console.WriteLine(firstnum + "+" + secondnum + "=" + result);
+            result = firstnum - secondnum;
+            Console.WriteLine(firstnum + "-" + secondnum + "=" + result);
+            result = firstnum * secondnum;
+            Console.WriteLine(firstnum + "*" + secondnum + "=" + result);
+            result = firstnum / secondnum;
+            Console.WriteLine(firstnum + "/" + secondnum + "=" + result);
+            result = firstnum % secondnum;
+            Console.WriteLine(firstnum + "%" + secondnum + "=" + result);
+        }
+    }
+}
